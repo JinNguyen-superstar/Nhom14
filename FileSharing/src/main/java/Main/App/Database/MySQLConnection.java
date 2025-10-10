3e59c0926ed76f142375d1c237e85fe32b43577a
ADMIN <ADMIN@DmK>
2025-10-10 09:43:43 +0700

    update 0.4 (complete connection data)

--- FileSharing/src/main/java/Main/App/Database/MySQLConnection.java
+++ FileSharing/src/main/java/Main/App/Database/MySQLConnection.java
@@ -3,11 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Main.App.Database;
-
+import java.sql.*;
 /**
  *
  * @author Asus
  */
 public class MySQLConnection {
-    
+    private static final String URL = "jdbc:mysql://10.0.1.176:3306/filesharingsystem?zeroDateBehavior=CONVERT_TO_NULL";
+    private static final String USER = "root";  
+    private static final String PASS = "your_password_here"; 
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
